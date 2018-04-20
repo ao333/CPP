9ba158615f7ff85516682ce577ce4964f8035c3d
ao333 <ao333@Ao.localdomain>
2018-04-20 22:45:19 +0100

    Remove the now ignored directory "some-directory"

--- Multitap/multitap.h
+++
@@ -1,8 +0,0 @@
-#include <fstream>
-using namespace std;
-
-int encode_character(char ch, char multitap[]);
-void encode(const char plaintext[], char multitap[]);
-char decode_character(const char multitap[]);
-void print_decode(const char* buffer, ostream& out, bool caps);
-void decode(istream& in, ostream& out);
--- Practice/Soundex/soundex.h
+++
@@ -1,8 +0,0 @@
-#ifndef SOUNDEX_H
-#define SOUNDEX_H
-
-void encode(const char* surname, char* soundex);
-bool compare(const char* one, const char* two);
-int count(const char* surname, const char* sentence);
-
-#endif
