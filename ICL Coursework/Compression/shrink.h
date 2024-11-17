fd5b425ed24ad3deac8d4ab3a6334ba7b3a50810
James Shen <ao_shen@hotmail.com>
2024-11-16 22:48:42 -0500

    update

--- ICL Coursework/Compression/shrink.h
+++
@@ -1,2 +0,0 @@
-int lookup(const char* word, const char* dictionary[]);
-void encode(const char* word, char compressed[100], const char* dictionary[]);
--- ICL Coursework/Practice/Exercises/3/conversions.h
+++
@@ -1,7 +0,0 @@
-#ifndef convert_H
-#define convert_H
-
-double celsius_of(int fah);
-double absolute_value_of(int fah);
-
-#endif
